8ab6854035bfea753c859bc4b94ff9bb8681c5fb
陈金雨 <chenjinyu@ovopark.com>
2023-07-25 10:29:13 +0800

    连续爬取多页

--- mySpider/mySpider/spiders/Douban.py
+++ mySpider/mySpider/spiders/Douban.py
@@ -14,39 +14,47 @@ class DoubanScrapy(scrapy.Spider):
     def parse(self, response):
         bookList = response.xpath('//div[@class="section-works"]/ul/li[@class="works-item "]')
         for book in bookList:
+           print(self.extractItem(book))
 
-            # 题目
-            item = DoubanBookItem()
-            item['title'] = book.xpath(".//span[@class='title-text']/span/text()").extract_first()
+        for i in range(2, 51):
+            url = "https://read.douban.com/category/100?sort=hot&page=" + str(i)
+            yield scrapy.Request(url, callback=self.parse)
 
-            # 作者和翻译员
-            authorList = book.xpath(".//div[@class='author']/a")
-            if len(authorList) > 1:
-                item['author'] = authorList[0].xpath("./span/span/text()").extract_first()
-                item['introduction'] = authorList[1].xpath("./span/span/text()").extract_first()
+    def extractItem(self, book):
 
-            else:
-                item['author'] = authorList[0].xpath("./span/span/text()").extract_first()
+        item = DoubanBookItem()
+        # 题目
+        item['title'] = book.xpath(".//span[@class='title-text']/span/text()").extract_first()
 
-            # 简介
-            item['introduction'] = book.xpath(".//a[@class='intro']/span/span/text()").extract_first()
+        # 作者和翻译员
+        authorList = book.xpath(".//div[@class='author']/a")
+        if len(authorList) > 1:
+            item['author'] = authorList[0].xpath("./span/span/text()").extract_first()
+            item['introduction'] = authorList[1].xpath("./span/span/text()").extract_first()
 
-            # 分类
-            item['kind'] = book.xpath(".//a[@class='kind-link']/text()").extract()
+        else:
+            item['author'] = authorList[0].xpath("./span/span/text()").extract_first()
 
-            #字数
-            item['wordCount'] = book.xpath(".//div[@class='sticky-info']/span[3]/text()").extract_first()
+        # 简介
+        item['introduction'] = book.xpath(".//a[@class='intro']/span/span/text()").extract_first()
 
-            # 价格
-            price = book.xpath(".//span[@class='price-tag']")
-            orgPrice = price.xpath("./s/text()").extract_first()
-            if orgPrice is None:
-                item['originalPrice'] = price.xpath("./text()").extract_first()
-            else:
-                item['originalPrice'] = orgPrice
-                item['discountPrice'] = price.xpath("./span[2]/text()").extract_first()
+        # 分类
+        item['kind'] = book.xpath(".//a[@class='kind-link']/text()").extract()
 
-            print(item)
+        # 字数
+        item['wordCount'] = book.xpath(".//div[@class='sticky-info']/span[3]/text()").extract_first()
+
+        # 价格
+        price = book.xpath(".//span[@class='price-tag']")
+        orgPrice = price.xpath("./s/text()").extract_first()
+        if orgPrice is None:
+            item['originalPrice'] = price.xpath("./text()").extract_first()
+        else:
+            item['originalPrice'] = orgPrice
+            item['discountPrice'] = price.xpath("./span[2]/text()").extract_first()
+
+        return item
+
 
 
 
